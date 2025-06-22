8e332dc5eb6177a818f8ad8ac427707757c4b388
tac <tac472@gmail.com>
2025-06-22 15:30:17 +0900

    基本的なベクトル演算の関数を追加

---
+++ modules/vector_cal/vector_cal.c
@@ -0,0 +1,49 @@
+#include <math.h>
+#include "../../miniRT.h"
+
+t_vec3 vec_new(double x, double y, double z)
+{
+	t_vec3 new;
+
+	new.x = x;
+	new.y = y;
+	new.z = z;
+	return (new);
+}
+
+t_vec3 vec_add(t_vec3 a, t_vec3 b)
+{
+	return (vec_new(a.x + b.x, a.y + b.y, a.z + b.z));
+}
+
+t_vec3 vec_sub(t_vec3 a, t_vec3 b)
+{
+	return (vec_new(a.x - b.x, a.y - b.y, a.z - b.z));
+}
+
+double vec_dot(t_vec3 a, t_vec3 b)
+{
+	return (a.x * b.x + a.y * b.y + a.z * b.z);	
+}
+
+t_vec3 vec_cross(t_vec3 a, t_vec3 b) 
+{
+	double f;
+	double s;
+	double t;
+
+	f = a.y * b.z - a.z * b.y;
+	s = a.z * b.x - a.x * b.z;
+	t = a.x * b.y - a.y * b.x;
+	return (vec_new(f, s, t));
+}
+
+t_vec3 vec_norm(t_vec3 v)
+{
+	double norm;
+
+	norm = sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+	if (norm == 0)
+		return (vec_new(0, 0, 0));
+	return (vec_new(v.x / norm, v.y / norm, v.z / norm));
+}
